1fd10d967e6ee43751729838a601b9a4e6537553
CW-Logothetis <cwlogo@pm.me>
2022-02-09 20:02:43 +0200

    add Mern mongo tutorial app.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import DataTable from './components/DataTable'
-  
+import React from "react";
+ 
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+ 
+// We import all the components we need in our app
+import Navbar from "./components/navbar";
+import RecordTable from "./components/RecordTable";
+import Edit from "./components/edit";
+// import Create from "./components/create";
+import FileUpload from "./components/FileUpload";
+ 
 const App = () => {
-  
-  return(
-    <DataTable />
-  )
-}
-  
-export default App
+ return (
+   <div>
+     {/* <Router> */}
+      <Navbar />
+        <Routes>
+          <Route exact path="/" element={<RecordTable />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/file-upload" element={<FileUpload />} />
+        </Routes>
+     {/* </Router> */}
+   </div>
+ );
+};
+ 
+export default App;
